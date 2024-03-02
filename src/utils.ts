5dc2e2f4a695eda8ecdc9988f883164ff791e887
frontweb-dev <frontend_web@kakao.com>
2024-03-03 01:58:37 +0900

    Create utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export const makeTime = (date: Date) => {
+  // 시간, 분, 초를 가져옵니다.
+  let hours: number | string = date.getHours();
+  let minutes: number | string = date.getMinutes();
+  let seconds: number | string = date.getSeconds();
+
+  // "오전" 또는 "오후"를 판별합니다.
+  const meridiem: string = hours >= 12 ? '오후' : '오전';
+
+  // 12시간 형식으로 시간을 변환합니다.
+  hours = hours % 12 || 12;
+
+  // 시간, 분, 초를 두 자리로 표시하기 위해 필요한 형식을 적용합니다.
+  hours = (hours < 10 ? '0' : '') + hours;
+  minutes = (minutes < 10 ? '0' : '') + minutes;
+  seconds = (seconds < 10 ? '0' : '') + seconds;
+
+  return `${meridiem} ${hours}:${minutes}:${seconds}`;
+};
