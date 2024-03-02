eda7e5675298d340fd6f138ff2101fce6d48cb30
frontweb-dev <frontend_web@kakao.com>
2024-03-03 01:58:49 +0900

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,27 +3,32 @@ import styled from 'styled-components';
 import Bedge from './Bedge';
 import { DocumentData } from 'firebase/firestore';
 import TaskModal from './Modal/TaskModal';
+import { ITask } from '../types';
+import { makeTime } from '../utils';
 
-const Card = (db: DocumentData) => {
+const Card = (task: ITask) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(true);
   };
+
   return (
     <>
       <Container onClick={handleClick}>
         <BedgeContainer>
-          <Bedge color='#E1AFD1'>{db.location}</Bedge>
-          <Bedge color='#FFE6E6'>{db.person}</Bedge>
+          <Bedge color='#E1AFD1'>{task.location}</Bedge>
+          <Bedge color='#FFE6E6'>{task.person}</Bedge>
         </BedgeContainer>
 
-        <HotelTitle>{db.hotel}</HotelTitle>
-        <MailTitle>{db.mail}</MailTitle>
-        <Progress>{db.state}</Progress>
+        <HotelTitle>{task.hotel}</HotelTitle>
+        <MailTitle>{task.mail}</MailTitle>
+        <Progress>{task.state}</Progress>
+        {task.state === '처리중' && <Progress>{makeTime(new Date(task.createdAt))}</Progress>}
+        {task.state === '처리완료' && <Progress>{makeTime(new Date(task.endedAt))}</Progress>}
       </Container>
 
-      {isOpen && <TaskModal setOpen={setIsOpen} db={db} />}
+      {isOpen && <TaskModal setOpen={setIsOpen} task={task} />}
     </>
   );
 };
