449bddbf9717c96364ca4feb89bd8b1400501894
frontweb-dev <frontend_web@kakao.com>
2024-03-03 01:58:39 +0900

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 export interface ITask {
-  createdAt: Date;
-  endedAt: Date;
+  createdAt: Date | string;
+  endedAt: Date | string;
   hotel: string;
   id: string;
   location: string;
   mail: string;
   option: string;
   person: string;
-  state: '미처리' | '처리중' | '처리 완료' | '질의중';
+  state: string;
   textarea: 'string';
 }
