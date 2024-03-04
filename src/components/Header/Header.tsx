e3ed999e15da01dfd6ebd39ca413331b948357a3
frontweb-dev <frontend_web@kakao.com>
2024-03-05 00:04:24 +0900

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { db } from '../../firebase';
@@ -14,18 +14,28 @@ const Header = ({ setState, total, done }: HeaderProps) => {
     setState(e.target.value);
   };
 
+  const handleClick = async () => {
+    const querySnapshot = await getDocs(collection(db, 'tasks'));
+    querySnapshot.forEach(async (doc) => {
+      await deleteDoc(doc.ref);
+    });
+  };
+
   return (
     <Container>
       <Title>Hotel Contents</Title>
       <Tracking>
-        <div>
-          <span>인입 건수 :</span>
-          <span>{total}</span>
-        </div>
-        <div>
-          <span>처리 완료 :</span>
-          <span>{done}</span>
-        </div>
+        <FlexContainer>
+          <div>
+            <span>인입 건수 :</span>
+            <span>{total}</span>
+          </div>
+          <div>
+            <span>처리 완료 :</span>
+            <span>{done}</span>
+          </div>
+        </FlexContainer>
+        <DeleteButton onClick={handleClick}>delete</DeleteButton>
 
         <Select>
           <select name='options' id='options' onChange={handleOptions}>
@@ -50,6 +60,22 @@ const Container = styled.div`
   margin-bottom: 20px;
 `;
 
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: bottom;
+  gap: 20px;
+`;
+
+const DeleteButton = styled.button`
+  width: 80px;
+  height: 30px;
+  color: #fff;
+  background-color: #ee3030;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+`;
+
 const Tracking = styled.div`
   display: flex;
   gap: 20px;
@@ -68,5 +94,4 @@ const Title = styled.h1`
 
 const Select = styled.div`
   margin-left: auto;
-  padding-right: 50px;
 `;
